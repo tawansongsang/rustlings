4c97d1729ccb7598875709692580af6399261e55
tawansongsang <tawansongsang.k@gmail.com>
2024-12-12 13:42:38 +0000

    finish using_as and from_into

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,31 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+
+        let split_str: Vec<&str> = s.split(',').collect();
+        if split_str.len() == 1 {
+            return Person::default();
+        }
+
+        let name = split_str[0];
+        if name.is_empty() {
+            return Person::default();
+        }
+
+        let age_parse = split_str[1].parse::<usize>();
+
+        match age_parse {
+            Ok(age) => Person {
+                name: name.to_string(),
+                age: age,
+            },
+            Err(_) => Person::default(),
+        }
     }
 }
 
