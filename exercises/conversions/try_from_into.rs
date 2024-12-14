f300356375e440291a4d63a011c258a467440a61
tawansongsang <tawansongsang.k@gmail.com>
2024-12-14 13:37:56 +0000

    finish try_from_into

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -27,8 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
 // integers, an array of three integers, and a slice of integers.
@@ -41,6 +39,16 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        match tuple {
+            (r, g, b) if (r >= 0 && r <= 255) && (g >= 0 && g <= 255) && (b >= 0 && b <= 255) => {
+                Ok(Color {
+                    red: r as u8,
+                    green: g as u8,
+                    blue: b as u8,
+                })
+            }
+            _ => Err(Self::Error::IntConversion),
+        }
     }
 }
 
@@ -48,6 +56,18 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let r = arr[0];
+        let g = arr[1];
+        let b = arr[2];
+        if (r >= 0 && r <= 255) && (g >= 0 && g <= 255) && (b >= 0 && b <= 255) {
+            return Ok(Color {
+                red: r as u8,
+                green: g as u8,
+                blue: b as u8,
+            });
+        }
+
+        Err(Self::Error::IntConversion)
     }
 }
 
@@ -55,6 +75,22 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(Self::Error::BadLen);
+        }
+
+        let r = slice[0];
+        let g = slice[1];
+        let b = slice[2];
+        if (r >= 0 && r <= 255) && (g >= 0 && g <= 255) && (b >= 0 && b <= 255) {
+            return Ok(Color {
+                red: r as u8,
+                green: g as u8,
+                blue: b as u8,
+            });
+        }
+
+        Err(Self::Error::IntConversion)
     }
 }
 
